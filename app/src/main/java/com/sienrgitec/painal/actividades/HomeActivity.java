d128d0e90a0c637014d0d791e3baead998c908b3
Alex181945 <estrada.mares.alejandro@gmail.com>
2020-04-18 00:21:43 -0500

    Lectura de Rest Giros y subgiros

--- app/src/main/java/com/sienrgitec/painal/actividades/HomeActivity.java
+++ app/src/main/java/com/sienrgitec/painal/actividades/HomeActivity.java
@@ -5,29 +5,19 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.gson.Gson;
 import com.sienrgitec.painal.R;
-import com.sienrgitec.painal.constante.Constantes;
 import com.sienrgitec.painal.pojo.error.ErrorUtils;
 import com.sienrgitec.painal.pojo.error.Errors;
 import com.sienrgitec.painal.pojo.respuesta.Respuesta;
-import com.sienrgitec.painal.pojo.sesion.Session;
 import com.sienrgitec.painal.servicio.Painal;
 import com.sienrgitec.painal.servicio.ServiceGenerator;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class HomeActivity extends AppCompatActivity {
--- app/src/main/java/com/sienrgitec/painal/servicio/ServiceGenerator.java
+++ app/src/main/java/com/sienrgitec/painal/servicio/ServiceGenerator.java
@@ -11,25 +11,21 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceGenerator {
 
-    private static String URL_BASE = Constantes.URL_BASE_WS_SINERGITEC;
-
-    public ServiceGenerator () {
-
-    }
+    private final static String URL_BASE = Constantes.URL_BASE_WS_SINERGITEC;
 
     private final static Gson gson = new GsonBuilder().setLenient().create();
 
-    private static Retrofit.Builder retro = new Retrofit.Builder()
+    private final static Retrofit.Builder retro = new Retrofit.Builder()
             .baseUrl(URL_BASE)
             .addConverterFactory(GsonConverterFactory.create(gson));
 
     public final static Retrofit retrofit = retro.build();
 
-    private static HttpLoggingInterceptor loggingInterceptor =
+    private final static HttpLoggingInterceptor loggingInterceptor =
             new HttpLoggingInterceptor()
                     .setLevel(HttpLoggingInterceptor.Level.BASIC);
 
-    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+    private final static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
 
     public static <S> S createService(Class<S> serviceClass) {
         if (!httpClient.interceptors().contains(loggingInterceptor)) {
