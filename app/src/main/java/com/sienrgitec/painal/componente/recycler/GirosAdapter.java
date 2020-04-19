0b3d8e61a52bc682c855352eef4064caa0f4f5bc
Alex181945Sinergitec <alejandro.estrada@sinergitec.mx>
2020-04-19 15:06:17 -0500

    Cambios de color al background del cardview

--- app/src/main/java/com/sienrgitec/painal/componente/recycler/GirosAdapter.java
+++ app/src/main/java/com/sienrgitec/painal/componente/recycler/GirosAdapter.java
@@ -1,11 +1,15 @@
 package com.sienrgitec.painal.componente.recycler;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
 
 import com.sienrgitec.painal.R;
 import com.sienrgitec.painal.componente.RVAdapter;
@@ -29,10 +33,12 @@ public class GirosAdapter extends RVAdapter<TtCtGiro_> {
     protected void bindView(TtCtGiro_ item, RVAdapter.ListViewHolder viewHolder) {
         if (item != null) {
 
+            CardView cardView = (CardView) viewHolder.getView(R.id.cv);
             TextView  title = (TextView)viewHolder.getView(R.id.title);
             TextView  subTitle = (TextView)viewHolder.getView(R.id.subTitle);
             ImageView photo = (ImageView)viewHolder.getView(R.id.photo);
 
+            cardView.setCardBackgroundColor(Color.RED);
             title.setText(item.getCGiro());
             subTitle.setText("");
             photo.setImageResource(R.drawable.ic_cafe);
