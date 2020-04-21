27aaa8d5a5f68ac0669a288253fb76e954ea76c1
Alex181945Sinergitec <alejandro.estrada@sinergitec.mx>
2020-04-20 22:39:50 -0500

    Menu

--- app/src/main/java/com/sienrgitec/painal/componente/recycler/GirosAdapter.java
+++ app/src/main/java/com/sienrgitec/painal/componente/recycler/GirosAdapter.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import androidx.cardview.widget.CardView;
 
 import com.sienrgitec.painal.R;
+import com.sienrgitec.painal.componente.DownloadImageTask;
 import com.sienrgitec.painal.componente.RVAdapter;
 import com.sienrgitec.painal.pojo.entity.TtCtGiro_;
 
@@ -41,7 +42,7 @@ public class GirosAdapter extends RVAdapter<TtCtGiro_> {
             cardView.setCardBackgroundColor(Color.RED);
             title.setText(item.getCGiro());
             subTitle.setText("");
-            photo.setImageResource(R.drawable.ic_cafe);
+            new DownloadImageTask(photo).execute("https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80");
         }
     }
 
