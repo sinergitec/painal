16b5f387b53089ff25e118d70732565590c64644
diazag <adrianadiaz@sinergitec.mx>
2020-04-25 14:20:54 -0500

    acomodo cardview y icono inicial

--- app/src/main/java/com/sienrgitec/painal/componente/recycler/SubGirosAdapter.java
+++ app/src/main/java/com/sienrgitec/painal/componente/recycler/SubGirosAdapter.java
@@ -13,7 +13,10 @@ import androidx.cardview.widget.CardView;
 import com.sienrgitec.painal.R;
 import com.sienrgitec.painal.componente.DownloadImageTask;
 import com.sienrgitec.painal.componente.RVAdapter;
+import com.sienrgitec.painal.constante.Constantes;
 import com.sienrgitec.painal.pojo.entity.TtCtSubGiro_;
+
+import java.util.Random;
 
 public class SubGirosAdapter extends RVAdapter<TtCtSubGiro_> {
     public SubGirosAdapter(Context context, OnViewHolderClick listener) {
@@ -35,7 +38,7 @@ public class SubGirosAdapter extends RVAdapter<TtCtSubGiro_> {
             TextView title = (TextView)viewHolder.getView(R.id.titleMosaic);
             ImageView photo = (ImageView)viewHolder.getView(R.id.imgMosaic);
 
-            cardView.setCardBackgroundColor(Color.RED);
+            cardView.setCardBackgroundColor(Color.parseColor(Constantes.ARRAY_COLORS[ new Random().nextInt(Constantes.ARRAY_COLORS.length) ]));
             title.setText(item.getCSubGiro());
             new DownloadImageTask(photo).execute("https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80");
         }
