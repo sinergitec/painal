4b185c33314f48092e80d4f73083d5752715078d
Alex181945Sinergitec <alejandro.estrada@sinergitec.mx>
2020-04-25 14:03:21 -0500

    Opcion de carga de subGiros y Proveedores

--- app/src/main/java/com/sienrgitec/painal/componente/recycler/SubGirosAdapter.java
+++ app/src/main/java/com/sienrgitec/painal/componente/recycler/SubGirosAdapter.java
@@ -1,20 +1,15 @@
 package com.sienrgitec.painal.componente.recycler;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.sienrgitec.painal.R;
-import com.sienrgitec.painal.componente.DownloadImageTask;
 import com.sienrgitec.painal.componente.RVAdapter;
 import com.sienrgitec.painal.pojo.entity.TtCtSubGiro_;
 import com.sienrgitec.painal.pojo.respuesta.Respuesta;
