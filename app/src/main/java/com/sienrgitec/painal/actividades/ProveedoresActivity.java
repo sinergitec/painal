521834e15d339e34116b20da9d1196570f608a59
Alex181945Sinergitec <alejandro.estrada@sinergitec.mx>
2020-05-01 20:39:41 -0500

    Articulos vista

--- app/src/main/java/com/sienrgitec/painal/actividades/ProveedoresActivity.java
+++ app/src/main/java/com/sienrgitec/painal/actividades/ProveedoresActivity.java
@@ -2,28 +2,18 @@ package com.sienrgitec.painal.actividades;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.sienrgitec.painal.R;
-import com.sienrgitec.painal.componente.RVAdapter;
 import com.sienrgitec.painal.componente.recycler.ProvClasifAdapter;
-import com.sienrgitec.painal.componente.recycler.SubGirosAdapter;
 import com.sienrgitec.painal.pojo.entity.TtCtCategoriaProv_;
 import com.sienrgitec.painal.pojo.entity.TtCtSubCategoriaProv;
-import com.sienrgitec.painal.pojo.respuesta.Respuesta;
-import com.sienrgitec.painal.servicio.Painal;
-import com.sienrgitec.painal.servicio.ServiceGenerator;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import retrofit2.Call;
 
 public class ProveedoresActivity  extends AppCompatActivity {
 
