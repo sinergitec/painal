1fc765ed53bf32562e01712b86e4c19150129be0
diazag <adrianadiaz@sinergitec.mx>
2020-07-01 00:03:38 -0500

    cambios en direcciones

--- app/src/main/java/com/sienrgitec/painal/componente/recycler/ListaDomicilioAdapter.java
+++ app/src/main/java/com/sienrgitec/painal/componente/recycler/ListaDomicilioAdapter.java
@@ -1,6 +1,7 @@
 package com.sienrgitec.painal.componente.recycler;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.sienrgitec.painal.R;
+import com.sienrgitec.painal.actividades.EliminaAutorizados;
+import com.sienrgitec.painal.actividades.EliminaDirecciones;
 import com.sienrgitec.painal.carrito.CarritoSingleton;
 import com.sienrgitec.painal.componente.RVAdapter;
 import com.sienrgitec.painal.pojo.entity.TtCtDomicilio_;
@@ -30,13 +33,21 @@ public class ListaDomicilioAdapter extends RVAdapter<TtCtDomicilio_> {
         if (item != null) {
             ImageView domicilioActual = (ImageView) viewHolder.getView(R.id.direccionPrincipal);
             TextView aliasDireccion = (TextView) viewHolder.getView(R.id.aliasDireccion);
-            ImageView editarDireccion = (ImageView) viewHolder.getView(R.id.imageView12);
-            ImageView elimianrDireccion = (ImageView) viewHolder.getView(R.id.borrarDomicilio);
+
 
             aliasDireccion.setText(item.getCCalle() + " " + (item.getCNumeroExt() != null ? item.getCNumeroExt() : "SN"));
             if(!item.getIDomicilio().equals(CarritoSingleton.getInstance().getDomicilioActual().getIDomicilio())){
                domicilioActual.setVisibility(View.INVISIBLE);
             }
+
+            ImageView elimianrDireccion = (ImageView) viewHolder.getView(R.id.borrarDomicilio);
+            elimianrDireccion.setOnClickListener(v ->{
+                Intent elimina = new Intent(viewHolder.getView().getContext(), EliminaDirecciones.class);
+                elimina.putExtra("objEliminar", item);
+                viewHolder.getView().getContext().startActivity(elimina);
+
+
+            });
 
         }
     }
