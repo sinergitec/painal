4ace3355dce3bc3baef2de834110eae667ef6f8c
diazag <adrianadiaz@sinergitec.mx>
2020-07-16 22:51:53 -0500

    Boton de pedido en el menu -Paginas de Historico de pedido-Seguimiento de pedido--Titlani Asignado- --Quitar la leyenda de "Algo para llevar"

---
+++ app/src/main/java/com/sienrgitec/painal/actividades/TitlaniAsignado.java
@@ -0,0 +1,15 @@
+package com.sienrgitec.painal.actividades;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sienrgitec.painal.R;
+
+public class TitlaniAsignado extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.titlani_asignado);
+    }
+}
