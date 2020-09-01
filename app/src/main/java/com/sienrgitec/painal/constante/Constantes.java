7b218e3ff93b2ea1b31c375f778562d1ce4a9453
diazag <adrianadiaz@sinergitec.mx>
2020-09-01 15:49:25 -0500

    correccion historico de pedido y seguimiento de pedido

--- app/src/main/java/com/sienrgitec/painal/constante/Constantes.java
+++ app/src/main/java/com/sienrgitec/painal/constante/Constantes.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 
 public class Constantes {
     //public static final String URL_BASE_WS = "http://escuela181945.herokuapp.com/api/";
-    public static final String URL_BASE_WS_SINERGITEC = "http://sinergitecdemo.ddns.net:8083/painal/rest/painalService/";
-    //public static final String URL_BASE_WS_SINERGITEC = "http://192.168.1.102:8083/painal/rest/painalService/";
+    //public static final String URL_BASE_WS_SINERGITEC = "http://sinergitecdemo.ddns.net:8083/painal/rest/painalService/";
+    public static final String URL_BASE_WS_SINERGITEC = "http://192.168.1.102:8083/painal/rest/painalService/";
     public static final String[] ARRAY_COLORS = {"#DCD3C1", "#F9FA81", "#FEC298", "#B9F6F9", "#96ABFE", "#BAFCBE", "#F4C9FD", "#FEACB6", "#F9D795"};
 
 
